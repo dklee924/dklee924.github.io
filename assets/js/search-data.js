4c4a2f6609d6ea81cd133f1a326ba26bf63ec8bd
dklee924 <59581671+dklee924@users.noreply.github.com>
2025-04-03 14:27:46 +0000

    Deploying to gh-pages from @ dklee924/dklee924.github.io@15a3ee946ab1f9555027a9c1f4442538eaa1cbb6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -434,21 +434,11 @@ ninja.data = [{
           description: "Deep reinforcement learning-based optimal maintenance strategy for large-scale infrastructure networks under seismic risk",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
